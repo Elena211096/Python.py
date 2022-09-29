3c255dc99c2cfeeb678eab2ab335b15c806dad18
Elena211096 <109298459+Elena211096@users.noreply.github.com>
2022-09-29 07:43:53 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,19 @@
+#Напишите программу для проверки истинности утверждения 
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+print('Введите X: ')
+x = int(input())
+print('Введите Y: ')
+y = int(input())
+print('Введите Z: ')
+z = int(input())
+
+
+left_part = not (x or y or z)
+right_part = not x and not y and not z
+
+
+if left_part == right_part:
+    print('Утверждение истинно')
+else:
+    print('Утверждение ложно')
